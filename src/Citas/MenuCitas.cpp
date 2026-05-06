b7e4d531c90061ff6ad34e65504be52d699e9a82
HuronClinch <onljsd@gmail.com>
2026-05-06 15:05:14 -0600

    Creacion de metodos(Crear, ver, editar, eliminar,buscar) para el menu de citas

--- src/Citas/MenuCitas.cpp
+++ src/Citas/MenuCitas.cpp
@@ -9,6 +9,93 @@ class MenuCitas
 private:
     CitasCRUD CRUD; // Crud para Citas
 
+    void menuCrear() // Ingresar datos para nueva cita
+    {
+        Cita cita; // Objeto de cita
+
+        cout << "\n\n\n~~~~~~ Crear nueva cita ~~~~~~\n"
+             << " Ingrese los siguintes datos:\n";
+        cout << "  ID Cita: ";
+        cin >> cita.idCita;
+        cout << "   ID Cliente: ";
+        cin >> cita.idCliente;
+        cout << "   Placa: ";
+        cin >> cita.placa;
+        cout << "   Fecha: ";
+        cin >> cita.fecha;
+        cout << "   Estado: ";
+        cin >> cita.estado;
+        cout << "   Costo: ";
+        cin >> cita.costo;
+
+        CRUD.crear(cita); // Crear objeto
+    }
+
+    void menuBuscar() // Buscar todas las citas
+    {
+        int id;
+        cout << "\n\n\n~~~~~~~ Buscar cita ~~~~~~~\n"
+             << "   Ingrese ID a buscar: ";
+        cin >> id;
+
+        Cita cita = CRUD.buscarPorId(id); // Objeto de cita encontrado
+
+        if (cita.idCita != -1) // Si se encontro cita
+            cout << "\n\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
+                 << "    La cita fue encontrada\n "
+                 << "       Placa: " << cita.placa << endl
+                 << "       Fecha: " << cita.fecha << endl
+                 << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
+        else // No eciste cita
+            cout << "\n\n\n~~~~~~~~~~~~~~~~~~~~~~~~\n"
+                 << "    No existe la cita\n"
+                 << "~~~~~~~~~~~~~~~~~~~~~~~~";
+    }
+
+    void editarCita() // Editar a usario por id
+    {
+        int id;
+
+        cout << "\n\n\n~~~~~~~~~~~~~ Editar cita ~~~~~~~~~~~~~\n"
+             << "   Ingrese ID de la cita a editar: ";
+        cin >> id;
+
+        Cita cita = CRUD.buscarPorId(id); // Objeto de cita
+
+        if (cita.idCita != -1) // Si de encuentra cita
+        {
+            cout << "\n\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
+                 << "    Editando Cita " << id << endl
+                 << "       Nuevo Estado: ";
+            cin >> cita.estado;
+            cout << "       Nuevo Costo: ";
+            cin >> cita.costo;
+
+            CRUD.editar(cita); // Guardar los cambios
+
+            cout << "\n\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
+                 << "    La cita se actualizo corectamente\n"
+                 << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
+        }
+        else
+            cout << "\n\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
+                 << "          La cita\n"
+                 << "   Cita no fue encontrada"
+                 << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
+    }
+
+    void eliminarCita() // Eliminar
+    {
+        int id;
+
+        cout << "\n\n\n~~~~~~~~~~~~~~~~~~~~~\n"
+             << "   Eliminando Cita " << id << endl
+             << "   ID a eliminar:";
+        cin >> id;
+
+        CRUD.eliminar(id); // Eliminar cita por id
+    }
+
 public:
     void menuCitas() // Menu del catalogo de citas
     {
@@ -42,25 +129,25 @@ public:
                 }
                 case 1: // Menu para crear Cita
                 {
-                    /*Logica*/
+                    menuCrear(); // Crear nuevo objeto de citas
                     break;
                 }
-                case 2: // Menu para listar Cita
-                    /*Logica*/
+                case 2:          // Menu para listar Cita
+                    CRUD.leer(); // Ver todas las citas
                     break;
                 case 3: // Menu para buscar Cita
                 {
-                    /*Logica*/
+                    menuBuscar(); // Buscar por id
                     break;
                 }
                 case 4: // Menu para editar Cita
                 {
-                    /*Logica*/
+                    editarCita(); // Editar cita por id
                     break;
                 }
                 case 5: // Menu para eliminar Cita
                 {
-                    /*Logica*/
+                    eliminarCita(); // Eliminar cita por id
                     break;
                 }
                 default:
